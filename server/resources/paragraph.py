fb4591d1fd405e7dd19c96933e62536b3f98cf06
Kaan Buke <kabuke13@gmail.com>
2025-08-19 19:18:38 +0200

    allow user to add a new paragraph

--- server/resources/paragraph.py
+++ server/resources/paragraph.py
@@ -9,3 +9,22 @@ class ParagraphList(Resource):
     def get(self):
         paragraphs = [paragraph.to_dict()for paragraph in ParagraphModel.query.all()]
         return paragraphs, 200
+    
+    def post(self):
+        json = request.get_json()
+
+        try:
+            new_paragraph = ParagraphModel(
+                title=json.get("title"),
+                text=json.get("text"),
+                img_1=json.get("img_1"),
+                img_2=json.get("img_2"),
+                project_id=json.get("project_id")
+            )
+            db.session.add(new_paragraph)
+            db.session.commit()
+            return new_paragraph.to_dict(), 201
+        except ValueError as e:
+            return{
+                "message": [str(e)]
+            }, 400
