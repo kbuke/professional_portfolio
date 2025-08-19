4acfa296fdc312120a53dcddb4ffe019ea0d131e
Kaan Buke <kabuke13@gmail.com>
2025-08-19 19:48:08 +0200

    allow user to add new tech

--- server/resources/tech.py
+++ server/resources/tech.py
@@ -3,9 +3,24 @@ from config import db
 from models.tech import TechModel
 
 from flask_restful import Resource
-from flask import session, request
+from flask import session, request, make_response
 
 class TechList(Resource):
     def get(self):
         tech = [tech_info.to_dict() for tech_info in TechModel.query.all()]
         return tech
+    
+    def post(self):
+        json=request.get_json()
+        try:
+            new_tech=TechModel(
+                name=json.get("name"),
+                img=json.get("img")
+            )
+            db.session.add(new_tech)
+            db.session.commit()
+            return new_tech.to_dict(), 201
+        except ValueError as e:
+            return{
+                "message": [str(e)]
+            }, 400
