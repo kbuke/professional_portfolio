7b68806b8b40b2ad63834531a367d2df638019f7
Kaan Buke <kabuke13@gmail.com>
2025-08-19 20:54:35 +0200

    allow a user to add a new qualification

--- server/resources/qualification.py
+++ server/resources/qualification.py
@@ -1,4 +1,5 @@
 from config import db 
+from datetime import datetime
 
 from models.qualification import QualificationModel
 
@@ -9,3 +10,26 @@ class QualificationList(Resource):
     def get(self):
         qualifications = [qualification.to_dict() for qualification in QualificationModel.query.all()]
         return qualifications
+    
+    def post(self):
+        json=request.get_json()
+        
+        selected_date = json.get("date")
+        if isinstance(selected_date, str):
+            selected_date=datetime.strptime(selected_date, "%Y-%m-%d").date()
+        
+        try:
+            new_qualification=QualificationModel(
+                title=json.get("title"),
+                img_1=json.get("img_1"),
+                img_2=json.get("img_2"),
+                date=selected_date,
+                institute_id=json.get("institute_id")
+            )
+            db.session.add(new_qualification)
+            db.session.commit()
+            return new_qualification.to_dict(), 201
+        except ValueError as e:
+            return{
+                "message": [str(e)]
+            }, 400
