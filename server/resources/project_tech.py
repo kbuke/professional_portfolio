9c3f594d5c542b03ae7e6e8f1594c8143af45f02
Kaan Buke <kabuke13@gmail.com>
2025-08-20 11:36:15 +0200

    allows the user to add new tech to a project

--- server/resources/project_tech.py
+++ server/resources/project_tech.py
@@ -3,9 +3,24 @@ from config import db
 from models.project_tech import ProjectTechModel
 
 from flask_restful import Resource
-from flask import session, request
+from flask import session, request, make_response
 
 class ProjectTechList(Resource):
     def get(self):
         project_tech = [project_tech_info.to_dict() for project_tech_info in ProjectTechModel.query.all()]
         return project_tech
+    
+    def post(self):
+        json=request.get_json()
+        try:
+            new_project_tech = ProjectTechModel(
+                project_id=json.get("project_id"),
+                tech_id=json.get("tech_id")
+            )
+            db.session.add(new_project_tech)
+            db.session.commit()
+            return new_project_tech.to_dict(), 201
+        except ValueError as e:
+            return{
+                "message": [str(e)]
+            }, 400
