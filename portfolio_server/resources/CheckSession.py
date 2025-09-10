a0407ddd9e561649ab5731fc6089e1e4f9f13a70
Kaan Buke <kabuke13@gmail.com>
2025-09-10 10:41:20 +0200

    allows a user to login

---
+++ portfolio_server/resources/CheckSession.py
@@ -0,0 +1,13 @@
+from flask import make_response, session, request
+from flask_restful import Resource
+from config import db 
+from models.User import UserModel
+
+class CheckSessionModel(Resource):
+    def get(self):
+        user_id = session.get("user_id")
+        if user_id:
+            user = UserModel.query.filter(UserModel.id == user_id).first()
+            if user:
+                return user.to_dict(), 200
+        return {"error": "Unauthorized user"}, 401
